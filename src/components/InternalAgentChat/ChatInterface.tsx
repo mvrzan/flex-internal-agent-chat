f3cde1fd6ad193f6a7b630b8120ea9409ca6ccef
mvrzan <mvrzan@icloud.com>
2023-08-07 12:30:53 -0400

    clear input field on send

--- src/components/InternalAgentChat/ChatInterface.tsx
+++ src/components/InternalAgentChat/ChatInterface.tsx
@@ -16,12 +16,14 @@ import {
   getConversationByUniqueName,
 } from '../utils/conversationsClient';
 import { SendIcon } from '@twilio-paste/icons/esm/SendIcon';
+import { Fragment } from 'react';
 
 const ChatInterface = ({ selectedAgent }: any) => {
-  const [conversation, setConversation] = useState(true);
+  const [isConversationEmpty, setIsConversationEmpty] = useState(true);
   const [messageList, setMessageList] = useState<any>([{}]);
   const [chat, setChat] = useState<any>();
   const [newMessage, setNewMessage] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     const getConversation = async () => {
@@ -133,96 +135,14 @@ const ChatInterface = ({ selectedAgent }: any) => {
 
   const conversationHandler = async (event: any) => {
     setNewMessage(event.target.value);
-    // const uniqueName = `${selectedAgent.contactUri}+${
-    //   conversationClient.user.identity
-    // }+${new Date().toJSON().slice(0, 10)}`;
-    // const conversationAttributes = { testAttribute: 'testAttribute' };
-    // const friendlyName = 'internal-chat';
-
-    // try {
-    //   // fetch Conversation by the unique name
-    //   const fetchedConversation = await getConversationByUniqueName(uniqueName);
-    //   console.log('fetchedConversation', fetchedConversation);
-    //   // if the Conversation exists, set all messages to read and remove all listeners
-    //   if (fetchedConversation !== null) {
-    //     console.log('Conversation found!');
-    //     // fetchedConversation.join();
-    //     fetchedConversation.setAllMessagesRead();
-    //     fetchedConversation.removeAllListeners();
-
-    //     // when the messages gets updated, update the messageList state
-    //     fetchedConversation.on('messageUpdated', updatedMessage => {
-    //       setMessageList((prevState: any[]) =>
-    //         prevState.map(prevMessage =>
-    //           prevMessage.sid === updatedMessage.message.sid
-    //             ? updatedMessage.message
-    //             : prevMessage
-    //         )
-    //       );
-    //     });
-
-    //     // when the messages gets added, update the messageList state
-    //     fetchedConversation.on('messageAdded', async message => {
-    //       console.log(message);
-    //       // const mediaUrl =
-    //       // message.type === "media" ? await message.attachedMedia : "";
-    //       // const mediaType = message.type === "media" ? message.attachedMedia : "";
-    //       const author = message.author;
-    //       const sid = message.sid;
-    //       const body = message.body;
-    //       const updatedMessage = {
-    //         ...message,
-    //         // mediaUrl,
-    //         // mediaType,
-    //         author,
-    //         sid,
-    //         body,
-    //       };
-    //       setMessageList((prevState: any[]) => [...prevState, updatedMessage]);
-    //       console.log(updatedMessage);
-    //     });
-    //     const res = await fetchedConversation.sendMessage('test');
-    //     console.log(res);
-    //     setChat(fetchedConversation);
-
-    //     //3. Load messages
-    //     const paginator = await fetchedConversation.getMessages(1000);
-    //     const messages = await Promise.all(
-    //       paginator.items.map(async s => {
-    //         // const mediaUrl =
-    //         //   s.type === "media" ? await s.media.getContentTemporaryUrl() : "";
-    //         // const mediaType = s.type === "media" ? s.media.contentType : "";
-    //         return {
-    //           author: s.author,
-    //           sid: s.sid,
-    //           body: s.body,
-    //           dateCreated: s.dateCreated,
-    //           attributes: s.attributes,
-    //           type: s.type,
-    //           // mediaUrl,
-    //           // mediaType,
-    //         };
-    //       })
-    //     );
-
-    //     console.log(messages);
-    //     setMessageList(messages);
-    //     // console.log(event);
-    //     // chat.sendMessage(event.target.value);
-    //   } else {
-    //     console.log('The conversation is null');
-    //   }
-    // } catch (error) {
-    //   if (error instanceof Error) {
-    //     console.error(error.message);
-    //   }
-    // }
+    setInputValue(event.target.value);
+    console.log(event);
   };
 
   const sendMessage = async () => {
     try {
-      const response = await chat.sendMessage(newMessage);
-      console.log('response', response);
+      await chat.sendMessage(newMessage);
+      setInputValue('');
     } catch (error) {
       console.error(error);
     }
@@ -232,30 +152,40 @@ const ChatInterface = ({ selectedAgent }: any) => {
     <Flex vertical grow width="100%" height="100%">
       <Flex grow width="100%">
         <Box width="100%" height="100%">
-          {conversation ? (
-            <NewConversationView selectedAgent={selectedAgent} />
+          {!isConversationEmpty ? (
+            <NewConversationView />
           ) : (
-            <>
-              <ChatLog>
-                <ChatMessage variant="inbound">
-                  <ChatBubble>Ahoy!</ChatBubble>
-                  <ChatMessageMeta aria-label="said by Gibby Radki 4 minutes ago">
-                    <ChatMessageMetaItem>
-                      Gibby Radki ・ 4 minutes ago
-                    </ChatMessageMetaItem>
-                  </ChatMessageMeta>
-                </ChatMessage>
-                <ChatMessage variant="outbound">
-                  <ChatBubble>Howdy!</ChatBubble>
-                  <ChatMessageMeta aria-label="said by you 2 minutes ago">
-                    <ChatMessageMetaItem>2 minutes ago</ChatMessageMetaItem>
-                  </ChatMessageMeta>
-                  <ChatMessageMeta aria-label="(read)">
-                    <ChatMessageMetaItem>Read</ChatMessageMetaItem>
-                  </ChatMessageMeta>
-                </ChatMessage>
-              </ChatLog>
-            </>
+            <ChatLog>
+              {messageList?.map((message: any) => {
+                return (
+                  <Fragment key={message.sid}>
+                    <ChatMessage
+                      variant={
+                        message.author === conversationClient.user.identity
+                          ? 'outbound'
+                          : 'inbound'
+                      }
+                    >
+                      <ChatBubble key={message.sid}>{message.body}</ChatBubble>
+                      <ChatMessageMeta aria-label="said by Gibby Radki 4 minutes ago">
+                        <ChatMessageMetaItem>
+                          {message.author} ・ 4 minutes ago
+                        </ChatMessageMetaItem>
+                      </ChatMessageMeta>
+                    </ChatMessage>
+                    <ChatMessage variant="inbound">
+                      <ChatBubble>Howdy!</ChatBubble>
+                      <ChatMessageMeta aria-label="said by you 2 minutes ago">
+                        <ChatMessageMetaItem>2 minutes ago</ChatMessageMetaItem>
+                      </ChatMessageMeta>
+                      <ChatMessageMeta aria-label="(read)">
+                        <ChatMessageMetaItem>Read</ChatMessageMetaItem>
+                      </ChatMessageMeta>
+                    </ChatMessage>
+                  </Fragment>
+                );
+              })}
+            </ChatLog>
           )}
         </Box>
       </Flex>
@@ -274,6 +204,7 @@ const ChatInterface = ({ selectedAgent }: any) => {
         >
           <Input
             type="text"
+            value={inputValue}
             onChange={conversationHandler}
             insertAfter={
               <Button variant="primary_icon" onClick={sendMessage}>
