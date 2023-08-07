8c5a5336904567f4be773a7d7b141fb2876d2695
mvrzan <mvrzan@icloud.com>
2023-08-07 11:59:56 -0400

    WIP add messages into the conversation

--- src/components/InternalAgentChat/ChatInterface.tsx
+++ src/components/InternalAgentChat/ChatInterface.tsx
@@ -24,110 +24,199 @@ const ChatInterface = ({ selectedAgent }: any) => {
   const [newMessage, setNewMessage] = useState('');
 
   useEffect(() => {
-    return () => {
-      chat.leave();
-    };
-  }, [chat]);
+    const getConversation = async () => {
+      const uniqueName = `${selectedAgent.contactUri}+${
+        conversationClient.user.identity
+      }+${new Date().toJSON().slice(0, 10)}`;
+      try {
+        const fetchedConversation =
+          await conversationClient.getConversationByUniqueName(uniqueName);
 
-  const conversationHandler = async (event: any) => {
-    const uniqueName = `${selectedAgent.contactUri}+${
-      conversationClient.user.identity
-    }+${new Date().toJSON().slice(0, 10)}`;
-    const conversationAttributes = { testAttribute: 'testAttribute' };
-    const friendlyName = 'internal-chat';
-    setNewMessage(event.target.value);
+        if (fetchedConversation !== null) {
+          console.log('Conversation found!', fetchedConversation);
+          fetchedConversation.status === 'notParticipating' &&
+            (await fetchedConversation.join());
+          fetchedConversation.setAllMessagesRead();
+          fetchedConversation.removeAllListeners();
 
-    try {
-      // fetch Conversation by the unique name
-      const fetchedConversation = await getConversationByUniqueName(uniqueName);
-      console.log('fetchedConversation', fetchedConversation);
-      // if the Conversation exists, set all messages to read and remove all listeners
-      if (fetchedConversation !== null) {
-        console.log('Conversation found!');
-        // fetchedConversation.join();
-        fetchedConversation.setAllMessagesRead();
-        fetchedConversation.removeAllListeners();
+          // when the messages gets updated, update the messageList state
+          fetchedConversation.on('messageUpdated', updatedMessage => {
+            setMessageList((prevState: any[]) =>
+              prevState.map(prevMessage =>
+                prevMessage.sid === updatedMessage.message.sid
+                  ? updatedMessage.message
+                  : prevMessage
+              )
+            );
+          });
 
-        // when the messages gets updated, update the messageList state
-        fetchedConversation.on('messageUpdated', updatedMessage => {
-          setMessageList((prevState: any[]) =>
-            prevState.map(prevMessage =>
-              prevMessage.sid === updatedMessage.message.sid
-                ? updatedMessage.message
-                : prevMessage
-            )
-          );
-        });
-
-        // when the messages gets added, update the messageList state
-        fetchedConversation.on('messageAdded', async message => {
-          console.log(message);
-          // const mediaUrl =
-          // message.type === "media" ? await message.attachedMedia : "";
-          // const mediaType = message.type === "media" ? message.attachedMedia : "";
-          const author = message.author;
-          const sid = message.sid;
-          const body = message.body;
-          const updatedMessage = {
-            ...message,
-            // mediaUrl,
-            // mediaType,
-            author,
-            sid,
-            body,
-          };
-          setMessageList((prevState: any[]) => [...prevState, updatedMessage]);
-          console.log(updatedMessage);
-        });
-        const res = await fetchedConversation.sendMessage('test');
-        console.log(res);
-        setChat(fetchedConversation);
-
-        //3. Load messages
-        const paginator = await fetchedConversation.getMessages(1000);
-        const messages = await Promise.all(
-          paginator.items.map(async s => {
+          // when the messages gets added, update the messageList state
+          fetchedConversation.on('messageAdded', async message => {
+            console.log(message);
             // const mediaUrl =
-            //   s.type === "media" ? await s.media.getContentTemporaryUrl() : "";
-            // const mediaType = s.type === "media" ? s.media.contentType : "";
-            return {
-              author: s.author,
-              sid: s.sid,
-              body: s.body,
-              dateCreated: s.dateCreated,
-              attributes: s.attributes,
-              type: s.type,
+            // message.type === "media" ? await message.attachedMedia : "";
+            // const mediaType = message.type === "media" ? message.attachedMedia : "";
+            const author = message.author;
+            const sid = message.sid;
+            const body = message.body;
+            const updatedMessage = {
+              ...message,
               // mediaUrl,
               // mediaType,
+              author,
+              sid,
+              body,
             };
-          })
-        );
+            setMessageList((prevState: any[]) => [
+              ...prevState,
+              updatedMessage,
+            ]);
+            console.log(updatedMessage);
+          });
+          // const res = await fetchedConversation.sendMessage('test');
+          // console.log(res);
+          setChat(fetchedConversation);
 
-        console.log(messages);
-        setMessageList(messages);
-        // console.log(event);
-        // chat.sendMessage(event.target.value);
-      } else {
-        console.log('The conversation is null');
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
+          //3. Load messages
+          const paginator = await fetchedConversation.getMessages(1000);
+          const messages = await Promise.all(
+            paginator.items.map(async s => {
+              // const mediaUrl =
+              //   s.type === "media" ? await s.media.getContentTemporaryUrl() : "";
+              // const mediaType = s.type === "media" ? s.media.contentType : "";
+              return {
+                author: s.author,
+                sid: s.sid,
+                body: s.body,
+                dateCreated: s.dateCreated,
+                attributes: s.attributes,
+                type: s.type,
+                // mediaUrl,
+                // mediaType,
+              };
+            })
+          );
 
-      try {
-        const createdConversation = await conversationClient.createConversation(
-          {
-            attributes: conversationAttributes,
-            friendlyName,
-            uniqueName,
-          }
-        );
-        console.log(createdConversation);
+          console.log(messages);
+          setMessageList(messages);
+
+          return fetchedConversation;
+        }
+        console.log('useEffect fetchedConversation', fetchedConversation);
       } catch (error) {
-        console.error(error);
+        if (error instanceof Error) {
+          if (error.message === 'Not Found') {
+            console.warn(
+              'The conversation was not found; creating a new conversation with uniqueName:',
+              uniqueName
+            );
+            await conversationClient.createConversation({
+              attributes: { testAttribute: 'testAttribute' },
+              friendlyName: 'internal-chat',
+              uniqueName,
+            });
+          }
+        }
       }
-    }
+    };
+    const fetchedConversation = getConversation();
+
+    return () => {
+      const leaveConversation = async () => {
+        const conversation = await fetchedConversation;
+        conversation?.removeAllListeners();
+        console.log('removingAllListeners');
+      };
+      leaveConversation();
+    };
+  }, [chat, selectedAgent.contactUri]);
+
+  const conversationHandler = async (event: any) => {
+    setNewMessage(event.target.value);
+    // const uniqueName = `${selectedAgent.contactUri}+${
+    //   conversationClient.user.identity
+    // }+${new Date().toJSON().slice(0, 10)}`;
+    // const conversationAttributes = { testAttribute: 'testAttribute' };
+    // const friendlyName = 'internal-chat';
+
+    // try {
+    //   // fetch Conversation by the unique name
+    //   const fetchedConversation = await getConversationByUniqueName(uniqueName);
+    //   console.log('fetchedConversation', fetchedConversation);
+    //   // if the Conversation exists, set all messages to read and remove all listeners
+    //   if (fetchedConversation !== null) {
+    //     console.log('Conversation found!');
+    //     // fetchedConversation.join();
+    //     fetchedConversation.setAllMessagesRead();
+    //     fetchedConversation.removeAllListeners();
+
+    //     // when the messages gets updated, update the messageList state
+    //     fetchedConversation.on('messageUpdated', updatedMessage => {
+    //       setMessageList((prevState: any[]) =>
+    //         prevState.map(prevMessage =>
+    //           prevMessage.sid === updatedMessage.message.sid
+    //             ? updatedMessage.message
+    //             : prevMessage
+    //         )
+    //       );
+    //     });
+
+    //     // when the messages gets added, update the messageList state
+    //     fetchedConversation.on('messageAdded', async message => {
+    //       console.log(message);
+    //       // const mediaUrl =
+    //       // message.type === "media" ? await message.attachedMedia : "";
+    //       // const mediaType = message.type === "media" ? message.attachedMedia : "";
+    //       const author = message.author;
+    //       const sid = message.sid;
+    //       const body = message.body;
+    //       const updatedMessage = {
+    //         ...message,
+    //         // mediaUrl,
+    //         // mediaType,
+    //         author,
+    //         sid,
+    //         body,
+    //       };
+    //       setMessageList((prevState: any[]) => [...prevState, updatedMessage]);
+    //       console.log(updatedMessage);
+    //     });
+    //     const res = await fetchedConversation.sendMessage('test');
+    //     console.log(res);
+    //     setChat(fetchedConversation);
+
+    //     //3. Load messages
+    //     const paginator = await fetchedConversation.getMessages(1000);
+    //     const messages = await Promise.all(
+    //       paginator.items.map(async s => {
+    //         // const mediaUrl =
+    //         //   s.type === "media" ? await s.media.getContentTemporaryUrl() : "";
+    //         // const mediaType = s.type === "media" ? s.media.contentType : "";
+    //         return {
+    //           author: s.author,
+    //           sid: s.sid,
+    //           body: s.body,
+    //           dateCreated: s.dateCreated,
+    //           attributes: s.attributes,
+    //           type: s.type,
+    //           // mediaUrl,
+    //           // mediaType,
+    //         };
+    //       })
+    //     );
+
+    //     console.log(messages);
+    //     setMessageList(messages);
+    //     // console.log(event);
+    //     // chat.sendMessage(event.target.value);
+    //   } else {
+    //     console.log('The conversation is null');
+    //   }
+    // } catch (error) {
+    //   if (error instanceof Error) {
+    //     console.error(error.message);
+    //   }
+    // }
   };
 
   const sendMessage = async () => {
