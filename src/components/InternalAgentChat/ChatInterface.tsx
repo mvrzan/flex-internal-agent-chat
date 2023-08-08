52520adf8b6aa50852a316e53d90752649790df2
mvrzan <mvrzan@icloud.com>
2023-08-08 17:05:44 -0400

    custom hook updates

--- src/components/InternalAgentChat/ChatInterface.tsx
+++ src/components/InternalAgentChat/ChatInterface.tsx
@@ -20,134 +20,12 @@ import { Fragment } from 'react';
 import useConversationsClient from '../utils/useConversationsClient';
 
 const ChatInterface = ({ selectedAgent }: any) => {
-  const [isConversationEmpty, setIsConversationEmpty] = useState(true);
-  const [messageList, setMessageList] = useState<any>([{}]);
-  const [chat, setChat] = useState<any>();
   const [newMessage, setNewMessage] = useState('');
   const [inputValue, setInputValue] = useState('');
-  const uniqueName = `${selectedAgent.contactUri}+${
-    conversationClient.user.identity
-  }+${new Date().toJSON().slice(0, 10)}`;
+  const uniqueName = `${selectedAgent.contactUri}+${conversationClient.user.identity}`;
 
-  const { conversationMessages, instantiatedConversation } =
+  const { conversationMessages, instantiatedConversation, isEmpty } =
     useConversationsClient(uniqueName);
-
-  useEffect(() => {
-    conversationMessages && conversationMessages.length > 0
-      ? setIsConversationEmpty(false)
-      : setIsConversationEmpty(true);
-  }, [instantiatedConversation, selectedAgent.contactUri]);
-
-  // useEffect(() => {
-  //   const getConversation = async () => {
-  //     const uniqueName = `${selectedAgent.contactUri}+${
-  //       conversationClient.user.identity
-  //     }+${new Date().toJSON().slice(0, 10)}`;
-  //     try {
-  //       const fetchedConversation =
-  //         await conversationClient.getConversationByUniqueName(uniqueName);
-
-  //       if (fetchedConversation !== null) {
-  //         fetchedConversation.status === 'notParticipating' &&
-  //           (await fetchedConversation.join());
-  //         fetchedConversation.setAllMessagesRead();
-  //         fetchedConversation.removeAllListeners();
-
-  //         // when the messages gets updated, update the messageList state
-  //         fetchedConversation.on('messageUpdated', updatedMessage => {
-  //           console.log('messageUpdated');
-  //           setMessageList((prevState: any[]) =>
-  //             prevState.map(prevMessage =>
-  //               prevMessage.sid === updatedMessage.message.sid
-  //                 ? updatedMessage.message
-  //                 : prevMessage
-  //             )
-  //           );
-  //         });
-
-  //         // when the messages gets added, update the messageList state
-  //         fetchedConversation.on('messageAdded', async message => {
-  //           console.log('messageAdded');
-  //           // const mediaUrl =
-  //           // message.type === "media" ? await message.attachedMedia : "";
-  //           // const mediaType = message.type === "media" ? message.attachedMedia : "";
-  //           const author = message.author;
-  //           const sid = message.sid;
-  //           const body = message.body;
-  //           const updatedMessage = {
-  //             ...message,
-  //             // mediaUrl,
-  //             // mediaType,
-  //             author,
-  //             sid,
-  //             body,
-  //           };
-  //           setMessageList((prevState: any[]) => [
-  //             ...prevState,
-  //             updatedMessage,
-  //           ]);
-  //           console.log(updatedMessage);
-  //         });
-  //         // const res = await fetchedConversation.sendMessage('test');
-  //         // console.log(res);
-  //         setChat(fetchedConversation);
-
-  //         //3. Load messages
-  //         const paginator = await fetchedConversation.getMessages(1000);
-  //         const messages = await Promise.all(
-  //           paginator.items.map(async s => {
-  //             // const mediaUrl =
-  //             //   s.type === "media" ? await s.media.getContentTemporaryUrl() : "";
-  //             // const mediaType = s.type === "media" ? s.media.contentType : "";
-  //             return {
-  //               author: s.author,
-  //               sid: s.sid,
-  //               body: s.body,
-  //               dateCreated: s.dateCreated,
-  //               attributes: s.attributes,
-  //               type: s.type,
-  //               // mediaUrl,
-  //               // mediaType,
-  //             };
-  //           })
-  //         );
-
-  //         setMessageList(messages);
-
-  //         messages.length > 0
-  //           ? setIsConversationEmpty(false)
-  //           : setIsConversationEmpty(true);
-
-  //         return fetchedConversation;
-  //       }
-  //     } catch (error) {
-  //       if (error instanceof Error) {
-  //         if (error.message === 'Not Found') {
-  //           console.warn(
-  //             'The conversation was not found; creating a new conversation with uniqueName:',
-  //             uniqueName
-  //           );
-  //           await conversationClient.createConversation({
-  //             attributes: { testAttribute: 'testAttribute' },
-  //             friendlyName: 'internal-chat',
-  //             uniqueName,
-  //           });
-  //         }
-  //       }
-  //     }
-  //   };
-  //   const fetchedConversation = getConversation();
-
-  //   // return () => {
-  //   //   const leaveConversation = async () => {
-  //   //     const conversation = await fetchedConversation;
-  //   //     conversation?.removeAllListeners();
-  //   //     // conversationClient.removeAllListeners();
-  //   //     console.log('removingAllListeners');
-  //   //   };
-  //   //   leaveConversation();
-  //   // };
-  // }, [chat, selectedAgent.contactUri]);
 
   const conversationHandler = async (event: any) => {
     setNewMessage(event.target.value);
@@ -156,7 +34,6 @@ const ChatInterface = ({ selectedAgent }: any) => {
 
   const sendMessage = async () => {
     try {
-      // await chat.sendMessage(newMessage);
       await instantiatedConversation.sendMessage(newMessage);
       setInputValue('');
     } catch (error) {
@@ -168,7 +45,7 @@ const ChatInterface = ({ selectedAgent }: any) => {
     <Flex vertical grow width="100%" height="100%">
       <Flex grow width="100%">
         <Box width="100%" height="100%">
-          {isConversationEmpty ? (
+          {isEmpty ? (
             <NewConversationView selectedAgent={selectedAgent} />
           ) : (
             <ChatLog>
--- src/components/utils/useConversationsClient.ts
+++ src/components/utils/useConversationsClient.ts
@@ -13,32 +13,59 @@ interface ConversationMessage {
 }
 
 const useConversationsClient = (uniqueName: string) => {
-  console.log('customHook!!!');
   const [conversationMessages, setConversationMessages] = useState<any>([{}]);
-  //   const [conversationMessages, setConversationMessages] =
-  //     useState<ConversationMessage[]>();
   const [instantiatedConversation, setInstantiatedConversation] =
     useState<any>();
   const conversationClient = Flex.Manager.getInstance().conversationsClient;
 
-  try {
-    const getConversation = async (uniqueName: string) => {
-      if (instantiatedConversation) {
-        console.log('conversationMessages', conversationMessages);
-        return { conversationMessages, instantiatedConversation };
+  useEffect(() => {
+    const getInstantiatedConversation = async (uniqueName: string) => {
+      try {
+        const fetchedConversation: Conversation =
+          await conversationClient.getConversationByUniqueName(uniqueName);
+        setInstantiatedConversation(fetchedConversation);
+        return fetchedConversation;
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.message === 'Not Found') {
+            console.warn(
+              'The conversation was not found; creating a new conversation with uniqueName:',
+              uniqueName
+            );
+            // Create a new Conversation since one does not exist
+            try {
+              const createNewConversation = async () => {
+                const newConversation =
+                  await conversationClient.createConversation({
+                    attributes: { testAttribute: 'testAttribute' },
+                    friendlyName: 'internal-chat',
+                    uniqueName,
+                  });
+                console.log('Conversation successfully created');
+                setInstantiatedConversation(newConversation);
+              };
+              createNewConversation();
+            } catch (error) {
+              // Catch and log an error in case we can't create a new Conversation
+              console.error(
+                'There was a problem creating a new conversation!',
+                error
+              );
+            }
+          }
+        }
       }
+    };
 
-      const fetchedConversation: Conversation =
-        await conversationClient.getConversationByUniqueName(uniqueName);
-      setInstantiatedConversation(fetchedConversation);
-      if (fetchedConversation !== null) {
+    const getConversation = async (fetchedConversation: any) => {
+      try {
         fetchedConversation.status === 'notParticipating' &&
           (await fetchedConversation.join());
         fetchedConversation.setAllMessagesRead();
         fetchedConversation.removeAllListeners();
 
         // when the messages gets updated, update the messageList state
-        fetchedConversation.on('messageUpdated', updatedMessage => {
+        fetchedConversation.on('messageUpdated', (updatedMessage: any) => {
           console.log('messageUpdated');
           setConversationMessages(
             (prevState: SetStateAction<ConversationMessage[] | undefined>) => {
@@ -55,7 +82,7 @@ const useConversationsClient = (uniqueName: string) => {
         });
 
         // when the messages gets added, update the messageList state
-        fetchedConversation.on('messageAdded', async message => {
+        fetchedConversation.on('messageAdded', async (message: any) => {
           console.log('messageAdded');
           // const mediaUrl =
           // message.type === "media" ? await message.attachedMedia : "";
@@ -88,7 +115,7 @@ const useConversationsClient = (uniqueName: string) => {
         //3. Load messages
         const paginator = await fetchedConversation.getMessages(1000);
         const messages = await Promise.all(
-          paginator.items.map(async s => {
+          paginator.items.map(async (s: any) => {
             // const mediaUrl =
             //   s.type === "media" ? await s.media.getContentTemporaryUrl() : "";
             // const mediaType = s.type === "media" ? s.media.contentType : "";
@@ -106,53 +133,26 @@ const useConversationsClient = (uniqueName: string) => {
         );
 
         setConversationMessages(messages);
+      } catch (error) {
+        console.error(
+          'an error has happened when updating conversation messages',
+          error
+        );
       }
     };
 
-    getConversation(uniqueName);
+    const init = async (uniqueName: string) => {
+      const fetchedConversation = await getInstantiatedConversation(uniqueName);
+      await getConversation(fetchedConversation);
+    };
+    init(uniqueName);
+  }, [uniqueName]);
 
-    if (instantiatedConversation && conversationMessages) {
-      return { conversationMessages, instantiatedConversation };
-    }
-
-    // if (conversationMessages !== undefined) {
-    //   return { conversationMessages, instantiatedConversation };
-    // } else {
-    //   return {};
-    // }
-    // return { conversationMessages, instantiatedConversation };
-  } catch (error) {
-    if (error instanceof Error) {
-      if (error.message === 'Not Found') {
-        console.warn(
-          'The conversation was not found; creating a new conversation with uniqueName:',
-          uniqueName
-        );
-        // Create a new Conversation since one does not exist
-        try {
-          const createNewConversation = async () => {
-            const newConversation = await conversationClient.createConversation(
-              {
-                attributes: { testAttribute: 'testAttribute' },
-                friendlyName: 'internal-chat',
-                uniqueName,
-              }
-            );
-
-            console.log('Conversation successfully created');
-          };
-          createNewConversation();
-        } catch (error) {
-          // Catch and log an error in case we can't create a new Conversation
-          console.error(
-            'There was a problem creating a new conversation!',
-            error
-          );
-        }
-      }
-    }
+  if (conversationMessages.length < 1) {
+    return { conversationMessages, instantiatedConversation, isEmpty: true };
+  } else {
+    return { conversationMessages, instantiatedConversation, isEmpty: false };
   }
-  return {};
 };
 
 export default useConversationsClient;
