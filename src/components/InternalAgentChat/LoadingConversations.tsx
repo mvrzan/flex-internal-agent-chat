08c6e0a671e0eaed60788ec2e7fbf0b1fd06c67f
mvrzan <mvrzan@icloud.com>
2023-08-22 15:14:41 -0400

    style the message loading screen

--- src/components/InternalAgentChat/LoadingConversations.tsx
+++ src/components/InternalAgentChat/LoadingConversations.tsx
@@ -1,5 +1,24 @@
+import { Spinner, Flex, Text } from '@twilio-paste/core';
+
 const LoadingConversations = () => {
-  return <div>Loading...</div>;
+  return (
+    <Flex
+      vertical
+      vAlignContent="center"
+      hAlignContent="center"
+      marginTop="space100"
+    >
+      <Spinner
+        decorative={false}
+        title="Loading chat"
+        size="sizeIcon110"
+        color="colorTextPrimaryStrongest"
+      />
+      <Text as="span" fontWeight="fontWeightMedium" marginTop="space40">
+        Loading your chat...
+      </Text>
+    </Flex>
+  );
 };
 
 export default LoadingConversations;
