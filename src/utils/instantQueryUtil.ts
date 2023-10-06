86ae3984604a6f5d82e9a755895ac3739d1876e7
mvrzan <mvrzan@icloud.com>
2023-10-06 09:40:35 -0400

    update instantQueryReturn object

--- src/utils/instantQueryUtil.ts
+++ src/utils/instantQueryUtil.ts
@@ -18,7 +18,8 @@ const instantQuerySearch = async (index: string, query: string) => {
 const getWorkers = async (query = '') => {
   const queryItems: any = await instantQuerySearch(
     'tr-worker',
-    `${query !== '' ? `${query}` : ''}`
+
+    `${query !== '' ? `data.attributes.full_name CONTAINS "${query}"` : ''}`
   );
 
   const responseWorkers = Object.keys(queryItems)
@@ -26,17 +27,16 @@ const getWorkers = async (query = '') => {
     .map(worker => {
       const { contact_uri, full_name, email, image_url, activity_name } =
         worker.attributes;
-      const workerSid = worker.worker_sid;
 
       return {
         firstName: full_name.split(' ')[0],
         lastName: full_name.split(' ')[1],
         fullName: full_name,
         contactUri: contact_uri.split(':')[1],
-        workerSid: workerSid,
+        workerSid: worker.worker_sid,
         email,
         imageUrl: image_url,
-        activityName: activity_name,
+        activityName: worker.activity_name,
       };
     });
 
