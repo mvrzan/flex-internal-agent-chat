a07c2f226e337a51c81714efe231b354763ef45e
mvrzan <mvrzan@icloud.com>
2023-11-23 12:24:20 -0500

    use implicit return

--- src/components/InternalAgentChat/SearchResults.tsx
+++ src/components/InternalAgentChat/SearchResults.tsx
@@ -17,33 +17,31 @@ const SearchResults = ({
   fullName,
   imageUrl,
   activityName,
-}: SearchResultsProps) => {
-  return (
-    <Box width="220px">
-      <Flex hAlignContent="between" vAlignContent="center" width="100%">
-        <Stack orientation="horizontal" spacing="space30">
-          <Avatar size="sizeIcon80" name={fullName} src={imageUrl} />
-          <Text as="span" marginRight="space40">
-            {fullName}
-          </Text>
-        </Stack>
-        <Tooltip text={activityName}>
-          <StatusBadge
-            as="span"
-            variant={
-              activityName === 'Available'
-                ? 'ProcessSuccess'
-                : activityName === 'Offline'
-                ? 'ProcessDisabled'
-                : 'ProcessWarning'
-            }
-          >
-            {' '}
-          </StatusBadge>
-        </Tooltip>
-      </Flex>
-    </Box>
-  );
-};
+}: SearchResultsProps) => (
+  <Box width="220px">
+    <Flex hAlignContent="between" vAlignContent="center" width="100%">
+      <Stack orientation="horizontal" spacing="space30">
+        <Avatar size="sizeIcon80" name={fullName} src={imageUrl} />
+        <Text as="span" marginRight="space40">
+          {fullName}
+        </Text>
+      </Stack>
+      <Tooltip text={activityName}>
+        <StatusBadge
+          as="span"
+          variant={
+            activityName === 'Available'
+              ? 'ProcessSuccess'
+              : activityName === 'Offline'
+              ? 'ProcessDisabled'
+              : 'ProcessWarning'
+          }
+        >
+          {' '}
+        </StatusBadge>
+      </Tooltip>
+    </Flex>
+  </Box>
+);
 
 export default SearchResults;
