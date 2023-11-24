ffdc9bd63df9a7daf9290c4928c1832ebe1b1958
mvrzan <mvrzan@icloud.com>
2023-11-24 16:45:28 -0500

    fix paste imports to minimize bundle size

--- src/components/InternalAgentChat/LoadingConversations.tsx
+++ src/components/InternalAgentChat/LoadingConversations.tsx
@@ -1,24 +1,25 @@
-import { Spinner, Flex, Text } from '@twilio-paste/core';
+import React from 'react';
+import { Spinner } from '@twilio-paste/core/spinner';
+import { Flex } from '@twilio-paste/core/flex';
+import { Text } from '@twilio-paste/text';
 
-const LoadingConversations = () => {
-  return (
-    <Flex
-      vertical
-      vAlignContent="center"
-      hAlignContent="center"
-      marginTop="space100"
-    >
-      <Spinner
-        decorative={false}
-        title="Loading chat"
-        size="sizeIcon110"
-        color="colorTextPrimaryStrongest"
-      />
-      <Text as="span" fontWeight="fontWeightMedium" marginTop="space40">
-        Loading your chat...
-      </Text>
-    </Flex>
-  );
-};
+const LoadingConversations = () => (
+  <Flex
+    vertical
+    vAlignContent="center"
+    hAlignContent="center"
+    marginTop="space100"
+  >
+    <Spinner
+      decorative={false}
+      title="Loading chat"
+      size="sizeIcon110"
+      color="colorTextPrimaryStrongest"
+    />
+    <Text as="span" fontWeight="fontWeightMedium" marginTop="space40">
+      Loading your chat...
+    </Text>
+  </Flex>
+);
 
 export default LoadingConversations;
