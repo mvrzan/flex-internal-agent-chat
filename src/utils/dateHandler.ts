337cbdc43c7ca78bb5f18f6796c83eef414ef95e
mvrzan <mvrzan@icloud.com>
2023-11-27 16:37:45 -0500

    add a new date formatting function

--- src/utils/dateHandler.ts
+++ src/utils/dateHandler.ts
@@ -4,3 +4,10 @@ export const dateFormatter = (date: Date) => {
 
   return localDateString;
 };
+
+export const dateStringFormatter = (date: Date) => {
+  const newDate = new Date(date);
+  const localDateString = newDate.toLocaleDateString();
+
+  return localDateString;
+};
